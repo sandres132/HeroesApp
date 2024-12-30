cd93065668611c6a1b5cfbed61219cf5aea62137
Andres Sorto <andresismaelsortozuniga@gmail.com>
2024-12-30 00:26:39 -0600

    Actualizacion del navbar y creacion de archivos de barril

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
-import {MarvelPage} from '../heroes/pages/MarvelPage'
-import {DcPage} from '../heroes/pages/DcPage'
-import {LoginPage} from '../auth/pages/LoginPage'
+import { Navbar } from '../ui'
+import {LoginPage} from '../auth'
+import {MarvelPage,DcPage} from '../heroes'
 
 export const AppRouter = () => {
   return (
     <>
+        <Navbar/>
         <Routes>
             <Route path='marvel' element={<MarvelPage/>}/>
             <Route path='dc' element={<DcPage/>}/>
